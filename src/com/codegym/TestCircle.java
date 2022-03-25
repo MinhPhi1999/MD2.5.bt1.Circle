b063af3679dbd891ef8713d3b1809711fa7cb11d
unknown <qquangmminh19999@gmail.com>
2022-03-25 13:42:09 +0700

    bt1

---
+++ src/com/codegym/TestCircle.java
@@ -0,0 +1,11 @@
+package com.codegym;
+
+public class TestCircle {
+
+    public static void main(String[] args) {
+	// write your code here
+        Circle circle = new Circle(100);
+        System.out.println(circle.getRadius());
+        System.out.println(circle.getArea());
+    }
+}
